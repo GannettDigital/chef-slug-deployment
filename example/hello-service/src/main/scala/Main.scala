1c27692965e7448a576c458975b665df6d5361cb
Eric Moritz <eric@themoritzfamily.com>
2014-12-20 04:10:52 -0500

    added the env vars

--- example/hello-service/src/main/scala/Main.scala
+++ example/hello-service/src/main/scala/Main.scala
@@ -1,15 +1,15 @@
 import spray.routing.SimpleRoutingApp
 import akka.actor._
-
+import util.Properties
 
 object Main extends App with SimpleRoutingApp {
   implicit val system = ActorSystem("chef-supervisord-fatjar-example")
   val port = 8000
 
-  startServer(interface = "0.0.0.0", port = port) {
+  startServer(interface = "0.0.0.0", port = Properties.envOrElse("PORT", "8000").toInt) {
     path("") {
       complete {
-        "Hello, World"
+        Properties.envOrElse("GREETING", "Hello, World")
       }
     }
   }
